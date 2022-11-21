76e86462ca5154b60c34a13dafcce996fa1f4c13
motifoye <goaway0137@gmail.com>
2022-11-21 11:14:44 +0700

    Добавьте файлы проекта.

---
+++ View/AuthWin.xaml.cs
@@ -0,0 +1,75 @@
+﻿using ManageStaffDBApp.Model;
+using System.Windows;
+
+namespace ManageStaffDBApp.View
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWin.xaml
+    /// </summary>
+    public partial class AuthWin : Window
+    {
+        public AuthWin()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsRegMode = false;
+        public bool IsAuthorize { get; private set; } = false;
+        private void bt_reg_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsRegMode)
+            {
+                if (!string.IsNullOrEmpty(tx_uname.Text) && !string.IsNullOrEmpty(tx_pass.Password) && tx_repass.Password.Equals(tx_pass.Password))
+                {
+                    MessageBox.Show(DataWorker.CreateEmploye(tx_uname.Text, tx_pass.Password));
+
+                    IsAuthorize = true;
+                    this.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("не всё введено");
+                }
+            }
+            else
+            {
+                IsRegMode = !IsRegMode;
+                tx_repass.Visibility = Visibility.Visible;
+                tb_repass.Visibility = Visibility.Visible;
+                lb_ti.Content = "Регистрация";
+            }
+        }
+
+        private void bt_in_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsRegMode)
+            {
+                IsRegMode = !IsRegMode;
+                tx_repass.Visibility = Visibility.Collapsed;
+                tb_repass.Visibility = Visibility.Collapsed;
+                lb_ti.Content = "Авторизация";
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(tx_uname.Text) && !string.IsNullOrEmpty(tx_pass.Password))
+                {
+                    if (DataWorker.CanLogIn(tx_uname.Text, tx_pass.Password))
+                    {
+                        IsAuthorize = true;
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("не знаю таких","иди нахуй");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("не всё введено");
+                }
+            }
+
+        }
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ManageStaffDBApp.ViewModel;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ManageStaffDBApp.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ListView AllDepartmentsView;
+        public static ListView AllPositionsView;
+        public static ListView AllUsersView;
+        public MainWindow()
+        {
+            if (!Authorize())
+            {
+                Application.Current.Shutdown(1);
+            }
+            InitializeComponent();
+            DataContext = new DataManageVM();
+            AllDepartmentsView = ViewAllDepartments;
+            AllPositionsView = ViewAllPositions;
+            AllUsersView = ViewAllUsers;
+        }
+
+        private bool Authorize()
+        {
+            AuthWin auth = new AuthWin();
+            auth.ShowDialog();
+            return auth.IsAuthorize;
+        }
+    }
+}
